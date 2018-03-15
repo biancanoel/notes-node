b5649c54b2d1bf4205a164e7ea6bdca1ff04e872
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-03-14 22:47:55 -0700

    commands can be run with shorter aliases

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,26 +6,51 @@ const _ = require('lodash');
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
-const argv = yargs.argv //the nice yargs formatted version of argv
+const titleObjects = {
+    describe: "Title of note",
+    demand: true, //tells yards the title is required for add command to work
+    alias: 't' //can use t instead of title
+};
+
+const bodyOptions = {
+    describe: "Contents of new note",
+    demand: true,
+    alias: "b"
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleObjects,
+        body: bodyOptions
+    })
+    .command('list', 'List all previosly created notes')
+    .command('read', 'Read a note', {
+        title: titleObjects
+    })
+    .command('remove', 'Delete a note', {
+        title: titleObjects
+    })
+    .help()
+    .argv //the nice yargs formatted version of argv
 var command = argv._[0];
 
-if (command==="add") {
-   var note = notes.addNote(argv.title, argv.body);
-   if (note) {
-    console.log('note created');
-    notes.logNote(note);
-   } else {
-    console.log('note title taken')
-   }
-   
-} else if (command==="list") {
+if (command === "add") {
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('note created');
+        notes.logNote(note);
+    } else {
+        console.log('note title taken')
+    }
+
+} else if (command === "list") {
     var allNotes = notes.getAll();
     console.log(`Printing ${allNotes.length} note(s)`);
-    allNotes.forEach( (note) => {
+    allNotes.forEach((note) => {
         notes.logNote(note);
     })
 
-} else if (command ==="read"){
+} else if (command === "read") {
     var note = notes.read(argv.title); //stores return value of read function (which will be note title and body)
     if (note) {
         console.log("note read");
@@ -33,8 +58,8 @@ if (command==="add") {
     } else {
         console.log('no note by that title could be found');
     }
-    
-} else if (command ==="remove"){
+
+} else if (command === "remove") {
     var noteRemoved = notes.remove(argv.title); //will be true or false based on returned value of remove function (t or f)
     var message = noteRemoved ? 'Note was removed' : "No note was removed, could not find note with specified title";
     console.log(message);
